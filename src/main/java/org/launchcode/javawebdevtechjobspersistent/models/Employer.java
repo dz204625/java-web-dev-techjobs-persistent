04b2d01e90e4a02660e677fb7560e33e4694eb31
Dan <dzhu0513@gmail.com>
2020-10-22 13:11:07 -0500

    assignment4 part1, 2 is done

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(min =1, max =300 , message = "Location must be between 5 to 300 characters long!")
+    @NotBlank
+    private String location;
 
+    public Employer(){
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String skillDescription;
+
+    public Skill() {
+    }
+
+    public Skill(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
+
+    public String getSkillDescription() {
+        return skillDescription;
+    }
+
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 }
